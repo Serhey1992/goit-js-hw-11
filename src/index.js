b5b80a2192132ed65762d4178e19e91d38af8498
Serhey1992 <makss92i@gmail.com>
2023-01-16 13:50:36 +0200

    +/-

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { imageApi } from "./js/fetch";
 import Notiflix from "notiflix";
-import { page } from "./js/fetch";
 import simpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let page = 1;
+let per_page = 40;
+let pages = 1;
+
+let inputText = '';
 
 const formEL = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
@@ -10,47 +16,66 @@ const loadBtn = document.querySelector('.load-more');
 formEL.addEventListener('submit', onSearch);
 loadBtn.addEventListener('click', onBtnClick);
 
-function onSearch(evt) {
+async function onSearch(evt) {
   evt.preventDefault();
-  galleryEl.innerHTML = '';
-
   const searchQuery = evt.currentTarget.elements.searchQuery.value
   .trim()
   .toLowerCase();
   
   if (!searchQuery) {
-    Notiflix.Notify.failure('Enter something');
-    return
+    loadBtn.classList.add('hidden');
+  };
+
+  if (inputText !== searchQuery) {
+    inputText = searchQuery;
+    cleanGalleryMarkup();
+  };
+
+  if (inputText || pages >= page) {
+    await onBtnClick();
+    simpleLightBox.refresh();
   }
     
-  imageApi(searchQuery).then(gallery => {
-    const { hits, totalHits } = gallery;
-    createGalleryMarkup(hits, totalHits);
-    loadBtn.hidden = false;
-    })
-    .catch(error => {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    })
 };
 
-function createGalleryMarkup(hits, totalHits) {
-    const markup = hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
+function cleanGalleryMarkup() {
+  galleryEl.innerHTML = '';
+  loadBtn.classList.add('hidden');
+  page = 1;
+}
+
+async function getInformation() {
+  try {
+    const gallery = await imageApi(inputText, page, per_page);
+    pages = Math.ceil(gallery.data.total / per_page);
+    const searchMassege = gallery.data;
+    console.log(gallery.data);
+    statusMsg(searchMassege, page, pages);
+    return searchMassege.hits;
+  } catch (error) {
+    console.log(error0);
+  }
+}
+
+
+function createGalleryMarkup(gallery) {
+    const markup = hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
     <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <a class="gallery__item" href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" class="photo-img" />
+    </a>
         <div class="info">
             <p class="info-item">
-            <b>Likes ${likes}</b>
+            <b>Likes</b>${likes}
             </p>
             <p class="info-item">
-            <b>Views ${views}</b>
+            <b>Views</b>${views}
             </p>
             <p class="info-item">
-            <b>Comments ${comments}</b>
+            <b>Comments</b>${comments}
             </p>
             <p class="info-item">
-            <b>Downloads ${downloads}</b>
+            <b>Downloads</b>${downloads}
             </p>
         </div>
     </div>`);
@@ -58,17 +83,33 @@ function createGalleryMarkup(hits, totalHits) {
     galleryEl.innerHTML = markup.join('');
 };
 
-function onBtnClick() {
-  page += 1;
-  loadBtn.hidden = true;
+function statusMsg(searchMassege, page, pages){ 
+  if (page === 1 && searchMassege.hits.length) {
+    Notiflix.Notify.success(`Hooray! We found ${searchMassege.totalHits} images.`);
+  };
 
-  imageApi(page).then(gallery => {
-    const { hits, totalHits } = gallery;
-    console.log(gallery);
-    createGalleryMarkup(hits, totalHits);
-    loadBtn.hidden = false;
-    // if (hits === totalHits) {
-    //   loadBtn.hidden = true;
-    // }
-    })
+  if (!searchMassege.total) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  };
+
+  if (page === pages) {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+  };
 }
+async function onBtnClick() {
+  const gallery = await getInformation();
+  loadBtn.classList.add('hidden');
+  createGalleryMarkup(gallery);
+  simpleLightBox.refresh();
+
+  if (pages > page) {
+    loadBtn.classList.remove('hidden');
+    page += 1;
+  }
+ }
+  
+
+const simpleLightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,22 +1,12 @@
 // https://pixabay.com/api/?key=32740167-9e99cf2539bcbb0df868cbe2b
 // &q = cat & image_type=photo & orientation=horizontal & safesearch=true & page=1 & per_page=40
 import axios from "axios";
-import Notiflix from "notiflix";
-export let page = 1;
-
 
 const BASE_URL = 'https://pixabay.com/api/';
 const key = '32740167-9e99cf2539bcbb0df868cbe2b';
-const parameters = '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+const parameters = '&image_type=photo&orientation=horizontal&safesearch=true&';
 
-export function imageApi(searchQuery) {
-  return fetch(`${BASE_URL}?key=${key}&q=${searchQuery}${parameters}&page=${page}`)
-    .then(response => {
-      if (!response.ok) {
-      throw new Error(console.log("Fail!"))
-      };
-
-      return response.json();
-  })
+export async function imageApi(searchQuery, page, per_page) {
+  return await axios.get(`${BASE_URL}?key=${key}&q=${searchQuery}${parameters}&page=${page}&per_page=${per_page}`)
    
 };
